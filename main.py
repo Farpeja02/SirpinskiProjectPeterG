63f345c2a714eb5318909296d55adb969a3314c5
Farpeja02 <74357609+Farpeja02@users.noreply.github.com>
2021-09-05 17:29:43 +0200

    Iteration 0.2: Added Object generation

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-
+import random
 
 Housekeeping = 0
 i = 0
-def GenerateStartDots():
+
+class ObejctOnScreen:
+
+    def RandomCords(self):
+        self.x = random.randint(0,1000)
+        self.y = random.randint(0,1000)
+    def DrawObj(self):
         pass
+def GenerateStartDots():
+        triangleCorner1 = ObejctOnScreen()
+        triangleCorner1.RandomCords()
+        triangleCorner2 = ObejctOnScreen()
+        triangleCorner2.RandomCords()
+        triangleCorner3 = ObejctOnScreen()
+        triangleCorner3.RandomCords()
 
 def GenerateStartObj():
-    pass
+    StartObj = ObejctOnScreen()
 
 def CycleOfDrawing():
     pass
 def ChangeObj():
     pass
-def DrawObj():
-    pass
+
 GenerateStartDots()
 GenerateStartObj()
+
 while i < 100000:
     ChangeObj()
     CycleOfDrawing()
-    DrawObj()
     i +=1
     print(i)
 
