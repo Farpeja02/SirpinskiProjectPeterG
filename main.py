47129a7b4ed9566084efa76a5cb448643e9f8a8f
Farpeja02 <74357609+Farpeja02@users.noreply.github.com>
2021-09-05 17:04:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+Housekeeping = 0
+i = 0
+def GenerateStartDots():
+        pass
+
+def GenerateStartObj():
+    pass
+
+def CycleOfDrawing():
+    pass
+def ChangeObj():
+    pass
+def DrawObj():
+    pass
+GenerateStartDots()
+GenerateStartObj()
+while i < 100000:
+    ChangeObj()
+    CycleOfDrawing()
+    DrawObj()
+    i +=1
+    print(i)
+
