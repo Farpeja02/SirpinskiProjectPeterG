c654f8fbd505229e6f4157d9838da56ffb29dbe7
Farpeja02 <74357609+Farpeja02@users.noreply.github.com>
2021-09-12 18:59:26 +0200

    Iteration 1,0: THe sirpinski triangle works, but leaves dots toward the circle.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ s = turtle.getscreen()
 t = turtle.Turtle()
 t.hideturtle()
 t.speed(100000)
-turtle.screensize(0,500)
+turtle.screensize(0,800)
 Housekeeping = 0
 i = 0
 n = 3
@@ -14,11 +14,11 @@ listOfCorners = []
 class ObejctOnScreen:
 
     def __init__(self):
-        self.x = random.randint(0,300)
-        self.y = random.randint(0,300)
+        self.x = random.randint(0,400)
+        self.y = random.randint(0,400)
     def DrawObj(self):
         t.goto(self.x,self.y)
-        t.dot(5)
+        t.dot(3)
 
 
 def GenerateStartDots():
@@ -35,16 +35,24 @@ def CycleOfDrawing():
 def ChangeObj():
     RandomCorner = random.randint(0,2)
     ChosenCorner = listOfCorners[RandomCorner]
-    StartObj.x = StartObj.x + ChosenCorner.x / 2
-    StartObj.y = StartObj.y + ChosenCorner.y / 2
+    StartObj.x = (StartObj.x + ChosenCorner.x) / 2
+    StartObj.y = (StartObj.y + ChosenCorner.y) / 2
+
+
 GenerateStartDots()
 GenerateStartObj()
-for corner in listOfCorners:
+c = 0
+for corner in listOfCorners * 2:
+    if c == 0:
+        t.penup()
+    else:
+        t.pendown()
     t.goto(corner.x,corner.y)
+    c += 1
 
 t.penup()
 
-while i < 1000:
+while i < 100000:
     ChangeObj()
     CycleOfDrawing()
-    i =1
+    i =+1
